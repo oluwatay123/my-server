1d39e4ca77dcda3428178df82b904fd221cbd329
Nathaniel Ezeanaka <ezeanakanath@gmail.com>
2024-07-01 12:36:52 +0100

    Complete project setup and implementation

---
+++ index.js
@@ -0,0 +1,66 @@
+//packages Initialization
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+// Getting geolocation related data
+const getGeodata = async () => {
+  try {
+    const response = await axios.get("https://ipapi.co/json");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching geo data:", error);
+    return {
+      city: "unknown location",
+      latitude: null,
+      longitude: null
+    };
+  }
+};
+
+// Getting current temperature data
+const getCurrentTemperature = async (lat, lon) => {
+  try {
+    const response = await axios.get(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`
+    );
+    return response.data;
+  } catch (error) {
+    console.log("Error fetching temperature:", error);
+    return { current_weather: { temperature: "unknown" } };
+  }
+};
+
+app.get("/api/hello", async function (req, res) {
+  const visitorName = req.query.visitor_name || "Guest";
+  const geoData = await getGeodata();
+  const location = geoData.city || "unknown location";
+  const ip = geoData.ip || req.ip;
+
+  let temperature = "unknown";
+  if (geoData.latitude && geoData.longitude) {
+    const tempData = await getCurrentTemperature(
+      geoData.latitude,
+      geoData.longitude
+    );
+    temperature = tempData.current_weather?.temperature ?? "unknown";
+  }
+
+  const greeting = `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celcius in ${location}.`;
+  res.json({
+    client_ip: ip,
+    location: location,
+    greeting: greeting
+  });
+});
+
+//Handles wrong routes
+app.all("*", (req, res) => {
+  res.status(404).send(`Cant find ${req.originalUrl} on the server`);
+});
+// Server listening
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
